d53c6adf01602441616543283f5807eb72994ca0
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2021-08-24 17:04:08 -0300

    Implementando componentes em projeto Angular

--- Blog Pessoal/blogpessoal/src/app/app.module.ts
+++ Blog Pessoal/blogpessoal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntarComponent } from './entar/entar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntarComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
