0e1226f1fbb2f3ab3ac9a338155b2ff8c20210c8
CarolLopesFavaretto <carolinelopesrdg@hotmail.com>
2021-08-25 09:06:43 -0300

    Implementando rotas ao projeto Angular

--- Blog Pessoal/blogpessoal/src/app/app-routing.module.ts
+++ Blog Pessoal/blogpessoal/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntarComponent } from './entar/entar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: "", redirectTo: "entar", pathMatch:"full"},
+
+  {path:"entar", component: EntarComponent},
+  {path: "cadastrar", component:CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
